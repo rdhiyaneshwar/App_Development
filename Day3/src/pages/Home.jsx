68f0438ce8a330336d106374a85a17b1e7ad36e5
Dhiyaneshwar R <100810927+rdhiyaneshwar@users.noreply.github.com>
2023-09-15 09:54:56 +0530

    Home.jsx

--- Day3/src/pages/Home.jsx
+++ Day3/src/pages/Home.jsx
@@ -17,15 +17,15 @@ function Home() {
         <section className="features">
        
           <div className="feature">
-            <h2 className='janice'>Find Care Services</h2>
+            <h2 className='dhiyanesh'>Find Care Services</h2>
             <p> Search for a variety of care services for your loved ones.</p>
           </div>
           <div className="feature">
-            <h2 className='janice'>Provider Directory</h2>
+            <h2 className='dhiyanesh'>Provider Directory</h2>
             <p>Explore a directory of trusted care providers in your area.</p>
           </div>
           <div className="feature">
-            <h2 className='janice'>Community Support</h2>
+            <h2 className='dhiyanesh'>Community Support</h2>
             <p>Join our community to share experiences and get support.</p>
           </div>
         </section>
